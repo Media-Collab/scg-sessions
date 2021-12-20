4e519c9d62cf7eade3f01acecbf1a322b6a0a2ca
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-20 17:17:41 -0500

    setting server, config middlewares and example routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,32 @@
  * Create server
  */
 const express = require("express");
+const cors = require("cors");
 
 /**
  * Create and initialize app
  * @constant app
  */
 const app = express();
+
+// settings config server
+app.set("port", process.env.PORT || 4000);
+
+// middleware define functions before enter to the routes
+app.use(cors()); // every time a request is received, the app will accepts
+app.use(express.json()); // the server knows JSON format
+
+// routes
+app.get("/api/users", (req, res) => {
+  res.json({
+    status: "Good, users routes",
+  });
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json({
+    status: "Good, notes routes",
+  });
+});
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -9,11 +9,9 @@ require("dotenv").config();
 const app = require("./app");
 require("./database");
 
-const PORT = process.env.PORT || 4000;
-
 async function main() {
-  await app.listen(PORT); // app.listen() async method
-  console.log(`Server listening in the port ${PORT}`);
+  await app.listen(app.get("port")); // app.listen() async method
+  console.log(`Server listening in the port ${app.get("port")}`);
 }
 
 main();
