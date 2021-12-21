a04793b4725fbefd65afe8c6a11ae127378c6726
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-20 20:46:50 -0500

    config components and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import NotesList from "./components/NotesList";
+import CreateNote from "./components/CreateNote";
+import CreateUser from "./components/CreateUser";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Router>
+      <Navigation />
+      <Route exact path="/" component={NotesList} />
+      <Route path="/edit/:id" component={CreateNote} />
+      <Route path="/create" component={CreateNote} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
 }
 
 export default App;
